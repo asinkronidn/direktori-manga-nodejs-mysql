6b4a8786cb837a706b35ea1691dfc811e9e107e8
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2023-10-16 21:24:19 +0700

    add dotenv

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const config = {
-    db: {
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "mangalisting"
-    }
-  };
-  module.exports = config;
+  db: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  }
+};
+module.exports = config;
